91d5fbffe526cb96799f0af819a0e1a8cf746b59
Tolya <trishin.anatoly2018@yandex.ru>
2020-01-07 15:05:06 +0300

    crate state component

--- src/App.js
+++ src/App.js
@@ -3,16 +3,29 @@ import Car from './Car/Car'
 import './App.css';
 
 class App extends Component {
+  
+  state ={
+    cars: [
+      {name:'Ford', year:2018 },
+      {name:'Audi', year:2016 },
+      {name:'Mazda', year:2010 }
+    ],
+    pageTitle: 'React components'
+  }
+
+
   render () {
     const divStyle = {
       textAlign: 'center'
     }
+     const cars = this.state.cars
     return (
       <div style = {divStyle}>
-        <h1>Hello world!</h1>
-        <Car name='Ford' year={2018}/>
-        <Car name='Audi' year={2016}/>
-        <Car name='Mazda' year={2010}/>
+        <h1>{this.state.pageTitle}</h1>
+        <Car name={cars[0].name} year={cars[0].year}/>
+        <Car name={cars[1].name} year={cars[1].year}/>
+        <Car name={cars[2].name} year={cars[2].year}/>
+     
       </div>
     )
   }
