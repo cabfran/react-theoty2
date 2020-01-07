cd8e3e209a4413aa4c1dd510e51f0b6e3af30671
Tolya <trishin.anatoly2018@yandex.ru>
2020-01-07 14:36:11 +0300

    parameters

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
-
+import Car from './Car/Car'
 import './App.css';
 
 class App extends Component {
   render () {
+    const divStyle = {
+      textAlign: 'center'
+    }
     return (
-      <div className ="App">
+      <div style = {divStyle}>
         <h1>Hello world!</h1>
+        <Car name='Ford' year={2018}/>
+        <Car name='Audi' year={2016}/>
+        <Car name='Mazda' year={2010}/>
       </div>
     )
   }
